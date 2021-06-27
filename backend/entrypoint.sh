12cf36a7cbc3087672c9cb94b5b33d36acaed765
Julien Jacquemet <jacquemet.julien@gmail.com>
2021-06-27 22:37:11 +0200

    add docker env for using in developpement

---
+++ backend/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Running docker entrypoint with these arguments: $@"
+echo "Current user: $(id)"
+echo "Current environment:"
+set
+echo
+
+if [ "$1" = "coreRest" ]; then
+	python manage.py makemigrations
+	python manage.py migrate
+	python manage.py collectstatic --noinput
+
+	if [ "$2" = "init" ]; then
+		echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
+	fi
+
+	exec python manage.py runserver 0.0.0.0:8000
+fi
+
+exec $@
